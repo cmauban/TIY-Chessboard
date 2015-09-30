bddeaa1212ec0a53a847ada943a2d30e0086963a
Camille Mauban <camille.mauban@gmail.com>
2015-09-30 19:02:12 -0400

    new things

--- js/tests.js
+++ js/tests.js
@@ -1,37 +1,59 @@
-var test = ('mocha').it,
-  expect = ('chai').expect;
-
-// test('initial', function(){
-//     expect(initial('R')).to.equal([0,0]);
-//       expect(initial([0,1])).to.be.a('N');
-
-console.log(game.tracer());
-console.log(game.board());
+// var test = ('mocha').it,
+//   expect = ('chai').expect;
+//
+//
+//
+// console.log(game.tracer());
+// console.log(game.board());
 
 
+(function(globals) {
+
+
+  it('should have a `game` in the `window`', function(){
+    chai.expect(window.game).to.be.equal(game);
+    chai.expect(game).is.an('object');
+    chai.expect(game.board).to.a('function');
+  });
+
+
+  it('should give me a big array as the board', function () {
+    var board = game.board();
+
+    chai.expect(board).to.be.an('array');
+    chai.expect(board).to.have.length(8);
+    chai.expect(board[0]).to.be.a('array');
+    chai.expect(board[0][0]).to.be.a('string');
+    chai.expect(board[0][0]).to.equal('R');
+  });
+
+  it('should be able to move pieces', function () {
+    // board[0][0] = board[0][1];
+    game.applyMove(
+      // { file: 'd', rank: 2 },
+      { rank: 6, to: 3 }, // from
+      // { file: 'd', rank: 4 }
+      { rank: 4, to: 3 } // to
+    );
+
+    var board = game.board();
+
+    chai.expect(board[6][3]).to.be.equal(null);
+    chai.expect(board[4][3]).to.be.equal('p');
+
+
+});
+
+
+// it('should be able to fast forward', function () {
+//   board[0][0] = board[0][1];
+//
+// });
+//
+// it.skip('should be able to advance to the next move', function () { //skips the test
+//
+//
 // });
 
 
-//data collected from game
-
-
-// What kind of thing is game.board...? What does it do?
-//  How about game.reset? What does it return? Any side-effects?
-//  Resetting a board with no moves is lame, HOW U EVEN game.applyMove?
-//  First move is free, next one costs $10.
-//  Just calling game.applyMove is cheating; game.next...!
-//  Now that you've got some moves, maybe game.reset is more interesting?
-//  You can game.next but can you game.prev...?
-//  Y'know, game.end is really a lot of game.next...
-//  That game.play, tho... How U even setInterval?
-
-/* 1. need to initalize the board (array)
-     - create variables for default settings
-     - size of board: 8x8
-     - colors?
-     - initial positions of pieces
-     - set the board
-   2. Make a move
-   3. Validate the move
-
-*/
+})(window || module && module.exports || this);
