3dd9dd8c36d7437655f3bff3cace785325010975
Camille Mauban <camille.mauban@gmail.com>
2015-10-04 16:55:48 -0400

    reset and fastforward working

--- js/controllers.js
+++ js/controllers.js
@@ -21,38 +21,38 @@ var $chessboard = $('.chessboard tbody');
 
 var gameboard = game.board();
 
-  // Controller for "next move"...
-  jQuery('#next').on('click', function(event){
 
+  // Controller for "next move"...
+jQuery('#next').on('click', function(event){
 
       console.log('next move');
       game.next(); //LOGS/APPLIES NEXT MOVE: Tell the Model -- `game` -- to advance to the next move...
-
+      event.preventDefault();
 
     // $('#next').once('click', function(){
-    //   event.preventDefault();
+
       jQuery(gameboard).each(function(rank, row){
+
         jQuery(row).each(function(file, piece){
           // var move = this.move; //current element in the loop.
+        // var click = gameboard[rank][file];
 
-      var clicks = 0;
-
-      if (clicks <= 2) {
-        clicks += 1;
-        $(this).show();
-
-      if (clicks == 1 && piece === 'p') {
+      if (piece === 'p') {
         $('tr.row-2 td:eq(3)').removeClass('wppawn'); //targets 3rd element from tr.row-2
         $('tr.row-4 td:eq(3)').addClass('wppawn');
+
       }
 
-      if (clicks == 2 && piece=== 'N') {
+
+      if (piece === 'N') {
         $('tr.row-8 td:eq(6)').removeClass('bpknight');
         $('tr.row-6 td:eq(5)').addClass('bpknight');
 
       }
-      return;
-} //end of dummy
+      // return;
+
+
+
       // if (piece === 'p') {
       //   $('tr.row-2 td:eq(2)').removeClass('wppawn');
       //   $('tr.row-4 td:eq(2)').addClass('wppawn');
@@ -96,7 +96,7 @@ var gameboard = game.board();
 
 
   // Controller for "previous move"...
-  jQuery('#previous').on('click', function(event){
+jQuery('#previous').on('click', function(event){
       console.log('previous move'); // TODO: Fire tracer bullet!
       game.prev(); // TODO: Tell the Model -- `game` -- to advance to the previous move...
 
@@ -104,7 +104,7 @@ var gameboard = game.board();
   });
 
   // Controller for "fast-forward"...
-  jQuery('#fast-forward').on('click', function(event){
+jQuery('#fast-forward').on('click', function(event){
       console.log('end game yas!');
       game.end(); //Tell the Model -- `game` -- to advance to the last move...
 
@@ -163,20 +163,61 @@ var gameboard = game.board();
 }); //END OF FAST-FORWARD BUTTON
 
   // Controller for anything else...
-  jQuery('#reset').on('click', function(event){
+jQuery('#reset').on('click', function(event){
       console.log('backward');  // Fire tracer bullet!
       game.reset(); // Tell the Model -- `game` -- to do something it knows how to do...
-
+      // Tell the View -- `.chessboard` -- to update the position of the pieces based on `game.board()`
       jQuery(gameboard).each(function(rank, row){
         jQuery(row).each(function(file, piece){
 
-          if (counter === 8){
-            return initial;
+          if (piece === 'p') {
+            $('tr.row-4 td:eq(3)').removeClass('wppawn');
+            $('tr.row-2 td:eq(3)').addClass('wppawn');
+          }
+
+          if (piece === 'N') {
+            $('tr.row-6 td:eq(5)').removeClass('bpknight');
+            $('tr.row-8 td:eq(6)').addClass('bpknight');
+          }
+
+          if (piece === 'p') {
+            $('tr.row-2 td:eq(2)').addClass('wppawn');
+            $('tr.row-4 td:eq(2)').removeClass('wppawn');
+          }
+
+          if (piece === 'P') {
+            $('tr.row-7 td:eq(4)').addClass('bppawn');
+            $('tr.row-6 td:eq(4)').removeClass('bppawn');
+          }
+
+          if (piece === 'p') {
+            $('tr.row-2 td:eq(6)').addClass('wppawn');
+            $('tr.row-3 td:eq(6)').removeClass('wppawn');
+          }
+
+          if (piece === 'P') {
+            $('tr.row-7 td:eq(3)').addClass('bppawn');
+            $('tr.row-5 td:eq(3)').removeClass('bppawn');
+          }
+
+          if (piece === 'b') {
+            $('tr.row-1 td:eq(5)').addClass('wpbishop');
+            $('tr.row-2 td:eq(6)').removeClass('wpbishop');
+          }
+
+          if (piece === 'B') {
+            $('tr.row-8 td:eq(5)').addClass('bpbishop');
+            $('tr.row-7 td:eq(4)').removeClass('bpbishop');
+          }
+
+          if (piece === 'n') {
+            $('tr.row-1 td:eq(6)').addClass('wpknight');
+            $('tr.row-3 td:eq(5)').removeClass('wpknight');
           }
 
 
     });
-  });    // TODO: Tell the View -- `.chessboard` -- to update the position of the pieces based on `game.board()`
+  });
 });
 
 // Am I supposed to recognize this?
